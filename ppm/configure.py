784d14d6acbff33aa81c944f2b9d35db62d0eae1
Quvonchbek Bobojonov <moorfoinfo@gmail.com>
2023-11-15 21:39:37 +0500

    feat: added pasers

---
+++ ppm/configure.py
@@ -0,0 +1,43 @@
+from argparse import ArgumentParser, Namespace
+
+parser = ArgumentParser(
+    description='Python Project manager parsers'
+)
+
+parser.add_argument(
+    '-v', '--version',
+    action='version',
+    version='PPM: 0.0.0',
+    help='Show version information'
+)
+
+parser.add_argument(
+    '-i', '--init',
+    action='store_true',
+    default=False,
+    help='Create a python project control',
+)
+
+parser.add_argument(
+    '-a', '--about',
+    action='store_true',
+    default=False,
+    help='About PPM',
+)
+
+parser.add_argument(
+    '-u', '--update',
+    action='store_true',
+    default=False,
+    help='Software update',
+)
+
+pip_parser = parser.add_subparsers(dest='command')
+pip_parser.add_parser(
+    'install',
+    help='Package installer',
+)
+
+if __name__ == '__main__':
+    args: Namespace = parser.parse_args()
+    print(args.command)
